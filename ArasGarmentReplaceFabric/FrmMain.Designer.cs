5b08e86d4c297c93adf3d63434bfe9ce2deb9ce4
ChenWes <chenxuhua0530@163.com>
2017-03-24 18:05:30 +0800

    project init

---
+++ ArasGarmentReplaceFabric/FrmMain.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace ArasGarmentReplaceFabric
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lab_password = new System.Windows.Forms.Label();
+            this.lab_DB = new System.Windows.Forms.Label();
+            this.lab_username = new System.Windows.Forms.Label();
+            this.lab_serverurl = new System.Windows.Forms.Label();
+            this.btn_disconnection = new System.Windows.Forms.Button();
+            this.btn_ConnectionAras = new System.Windows.Forms.Button();
+            this.txt_username = new System.Windows.Forms.TextBox();
+            this.txt_password = new System.Windows.Forms.TextBox();
+            this.txt_DB = new System.Windows.Forms.TextBox();
+            this.txt_serverurl = new System.Windows.Forms.TextBox();
+            this.tre_Item = new System.Windows.Forms.TreeView();
+            this.imageLiarbry = new System.Windows.Forms.ImageList(this.components);
+            this.txt_SearchAML = new System.Windows.Forms.TextBox();
+            this.txt_DataList = new System.Windows.Forms.TextBox();
+            this.btn_StartReplace = new System.Windows.Forms.Button();
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.pro_CheckItem = new System.Windows.Forms.ProgressBar();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.lab_password);
+            this.panel1.Controls.Add(this.lab_DB);
+            this.panel1.Controls.Add(this.lab_username);
+            this.panel1.Controls.Add(this.lab_serverurl);
+            this.panel1.Controls.Add(this.btn_disconnection);
+            this.panel1.Controls.Add(this.btn_ConnectionAras);
+            this.panel1.Controls.Add(this.txt_username);
+            this.panel1.Controls.Add(this.txt_password);
+            this.panel1.Controls.Add(this.txt_DB);
+            this.panel1.Controls.Add(this.txt_serverurl);
+            this.panel1.Location = new System.Drawing.Point(11, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(824, 74);
+            this.panel1.TabIndex = 2;
+            // 
+            // lab_password
+            // 
+            this.lab_password.AutoSize = true;
+            this.lab_password.Location = new System.Drawing.Point(364, 44);
+            this.lab_password.Name = "lab_password";
+            this.lab_password.Size = new System.Drawing.Size(53, 12);
+            this.lab_password.TabIndex = 2;
+            this.lab_password.Text = "PassWord";
+            // 
+            // lab_DB
+            // 
+            this.lab_DB.AutoSize = true;
+            this.lab_DB.Location = new System.Drawing.Point(364, 17);
+            this.lab_DB.Name = "lab_DB";
+            this.lab_DB.Size = new System.Drawing.Size(41, 12);
+            this.lab_DB.TabIndex = 2;
+            this.lab_DB.Text = "DBName";
+            // 
+            // lab_username
+            // 
+            this.lab_username.AutoSize = true;
+            this.lab_username.Location = new System.Drawing.Point(17, 44);
+            this.lab_username.Name = "lab_username";
+            this.lab_username.Size = new System.Drawing.Size(53, 12);
+            this.lab_username.TabIndex = 2;
+            this.lab_username.Text = "UserName";
+            // 
+            // lab_serverurl
+            // 
+            this.lab_serverurl.AutoSize = true;
+            this.lab_serverurl.Location = new System.Drawing.Point(17, 17);
+            this.lab_serverurl.Name = "lab_serverurl";
+            this.lab_serverurl.Size = new System.Drawing.Size(59, 12);
+            this.lab_serverurl.TabIndex = 2;
+            this.lab_serverurl.Text = "ServerUrl";
+            // 
+            // btn_disconnection
+            // 
+            this.btn_disconnection.Location = new System.Drawing.Point(715, 39);
+            this.btn_disconnection.Name = "btn_disconnection";
+            this.btn_disconnection.Size = new System.Drawing.Size(87, 23);
+            this.btn_disconnection.TabIndex = 1;
+            this.btn_disconnection.Text = "Disconnection";
+            this.btn_disconnection.UseVisualStyleBackColor = true;
+            // 
+            // btn_ConnectionAras
+            // 
+            this.btn_ConnectionAras.Location = new System.Drawing.Point(715, 12);
+            this.btn_ConnectionAras.Name = "btn_ConnectionAras";
+            this.btn_ConnectionAras.Size = new System.Drawing.Size(87, 23);
+            this.btn_ConnectionAras.TabIndex = 1;
+            this.btn_ConnectionAras.Text = "Connection";
+            this.btn_ConnectionAras.UseVisualStyleBackColor = true;
+            this.btn_ConnectionAras.Click += new System.EventHandler(this.btn_ConnectionAras_Click);
+            // 
+            // txt_username
+            // 
+            this.txt_username.Location = new System.Drawing.Point(82, 41);
+            this.txt_username.Name = "txt_username";
+            this.txt_username.Size = new System.Drawing.Size(254, 21);
+            this.txt_username.TabIndex = 0;
+            this.txt_username.Text = "admin";
+            // 
+            // txt_password
+            // 
+            this.txt_password.Location = new System.Drawing.Point(423, 41);
+            this.txt_password.Name = "txt_password";
+            this.txt_password.PasswordChar = '*';
+            this.txt_password.Size = new System.Drawing.Size(254, 21);
+            this.txt_password.TabIndex = 0;
+            this.txt_password.Text = "innovator";
+            // 
+            // txt_DB
+            // 
+            this.txt_DB.Location = new System.Drawing.Point(423, 14);
+            this.txt_DB.Name = "txt_DB";
+            this.txt_DB.Size = new System.Drawing.Size(254, 21);
+            this.txt_DB.TabIndex = 0;
+            this.txt_DB.Text = "Esquel_PLM_SIT";
+            // 
+            // txt_serverurl
+            // 
+            this.txt_serverurl.Location = new System.Drawing.Point(82, 14);
+            this.txt_serverurl.Name = "txt_serverurl";
+            this.txt_serverurl.Size = new System.Drawing.Size(254, 21);
+            this.txt_serverurl.TabIndex = 0;
+            this.txt_serverurl.Text = "http://10.20.2.30/InnovatorServer/";
+            // 
+            // tre_Item
+            // 
+            this.tre_Item.ImageIndex = 0;
+            this.tre_Item.ImageList = this.imageLiarbry;
+            this.tre_Item.Location = new System.Drawing.Point(11, 185);
+            this.tre_Item.Name = "tre_Item";
+            this.tre_Item.SelectedImageIndex = 0;
+            this.tre_Item.Size = new System.Drawing.Size(402, 398);
+            this.tre_Item.TabIndex = 8;
+            // 
+            // imageLiarbry
+            // 
+            this.imageLiarbry.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageLiarbry.ImageStream")));
+            this.imageLiarbry.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageLiarbry.Images.SetKeyName(0, "wait.png");
+            this.imageLiarbry.Images.SetKeyName(1, "start.png");
+            this.imageLiarbry.Images.SetKeyName(2, "stop.png");
+            this.imageLiarbry.Images.SetKeyName(3, "complete.png");
+            // 
+            // txt_SearchAML
+            // 
+            this.txt_SearchAML.Location = new System.Drawing.Point(12, 113);
+            this.txt_SearchAML.Multiline = true;
+            this.txt_SearchAML.Name = "txt_SearchAML";
+            this.txt_SearchAML.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txt_SearchAML.Size = new System.Drawing.Size(401, 66);
+            this.txt_SearchAML.TabIndex = 9;
+            this.txt_SearchAML.Text = resources.GetString("txt_SearchAML.Text");
+            // 
+            // txt_DataList
+            // 
+            this.txt_DataList.Location = new System.Drawing.Point(423, 185);
+            this.txt_DataList.Multiline = true;
+            this.txt_DataList.Name = "txt_DataList";
+            this.txt_DataList.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txt_DataList.Size = new System.Drawing.Size(409, 398);
+            this.txt_DataList.TabIndex = 10;
+            // 
+            // btn_StartReplace
+            // 
+            this.btn_StartReplace.Location = new System.Drawing.Point(568, 143);
+            this.btn_StartReplace.Name = "btn_StartReplace";
+            this.btn_StartReplace.Size = new System.Drawing.Size(121, 19);
+            this.btn_StartReplace.TabIndex = 11;
+            this.btn_StartReplace.Text = "Start Replace";
+            this.btn_StartReplace.UseVisualStyleBackColor = true;
+            this.btn_StartReplace.Click += new System.EventHandler(this.btn_StartReplace_Click);
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.Location = new System.Drawing.Point(441, 143);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(121, 19);
+            this.btn_Start.TabIndex = 12;
+            this.btn_Start.Text = "Start Check";
+            this.btn_Start.UseVisualStyleBackColor = true;
+            this.btn_Start.Click += new System.EventHandler(this.btn_Start_Click);
+            // 
+            // pro_CheckItem
+            // 
+            this.pro_CheckItem.Location = new System.Drawing.Point(12, 92);
+            this.pro_CheckItem.Name = "pro_CheckItem";
+            this.pro_CheckItem.Size = new System.Drawing.Size(823, 16);
+            this.pro_CheckItem.TabIndex = 13;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(844, 588);
+            this.Controls.Add(this.pro_CheckItem);
+            this.Controls.Add(this.btn_StartReplace);
+            this.Controls.Add(this.btn_Start);
+            this.Controls.Add(this.txt_DataList);
+            this.Controls.Add(this.txt_SearchAML);
+            this.Controls.Add(this.tre_Item);
+            this.Controls.Add(this.panel1);
+            this.Name = "FrmMain";
+            this.Text = "Aras Garment Style Replace Fabric";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lab_password;
+        private System.Windows.Forms.Label lab_DB;
+        private System.Windows.Forms.Label lab_username;
+        private System.Windows.Forms.Label lab_serverurl;
+        private System.Windows.Forms.Button btn_disconnection;
+        private System.Windows.Forms.Button btn_ConnectionAras;
+        private System.Windows.Forms.TextBox txt_username;
+        private System.Windows.Forms.TextBox txt_password;
+        private System.Windows.Forms.TextBox txt_DB;
+        private System.Windows.Forms.TextBox txt_serverurl;
+        private System.Windows.Forms.TreeView tre_Item;
+        private System.Windows.Forms.TextBox txt_SearchAML;
+        private System.Windows.Forms.TextBox txt_DataList;
+        private System.Windows.Forms.Button btn_StartReplace;
+        private System.Windows.Forms.Button btn_Start;
+        private System.Windows.Forms.ProgressBar pro_CheckItem;
+        private System.Windows.Forms.ImageList imageLiarbry;
+    }
+}
+
---
+++ ArasGarmentReplaceFabric/FrmMain.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Aras.IOM;
+
+namespace ArasGarmentReplaceFabric
+{
+    public partial class FrmMain : Form
+    {
+        private HttpServerConnection mc_conn = null;
+        private Innovator mc_innovator = null;
+
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void GetConnection(string l_serverurl, string l_db, string l_username, string l_password)
+        {
+            mc_conn = IomFactory.CreateHttpServerConnection(l_serverurl.Trim(), l_db.Trim(), l_username.Trim(), Innovator.ScalcMD5(l_password.Trim()));
+        }
+
+        private void GetInnovator()
+        {
+            mc_innovator = IomFactory.CreateInnovator(mc_conn);
+        }
+
+        private void ShowError(string errormessage)
+        {
+            MessageBox.Show(errormessage);
+        }
+
+        private void SettingProcess(int maxValue, int CurrentValue)
+        {
+            decimal getvalue = CurrentValue * 100 / maxValue;
+            pro_CheckItem.Value = (int)getvalue;
+            pro_CheckItem.Refresh();
+        }
+
+        private void SettingConnectionButton(bool bln_ConnectionFlag)
+        {
+            btn_ConnectionAras.Enabled = !bln_ConnectionFlag;
+            btn_disconnection.Enabled = bln_ConnectionFlag;
+
+            txt_serverurl.Enabled = !bln_ConnectionFlag;
+            txt_DB.Enabled = !bln_ConnectionFlag;
+            txt_username.Enabled = !bln_ConnectionFlag;
+            txt_password.Enabled = !bln_ConnectionFlag;
+
+            btn_Start.Enabled = bln_ConnectionFlag;
+
+            txt_SearchAML.Enabled = bln_ConnectionFlag;
+
+            if (!bln_ConnectionFlag)
+            {
+                tre_Item.Nodes.Clear();
+            }
+        }
+
+        private void SettingCheckItemButton(bool bln_CheckItemFlag)
+        {
+            btn_Start.Enabled = !bln_CheckItemFlag;
+
+            btn_StartReplace.Enabled = bln_CheckItemFlag;
+            tre_Item.Enabled = bln_CheckItemFlag;
+        }
+
+        //------------------------------------------------------------------------------------------------------
+
+        private void btn_ConnectionAras_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(txt_serverurl.Text.Trim()))
+                {
+                    throw new Exception("Server Url Is Null Or Empty !");
+                }
+
+                if (string.IsNullOrEmpty(txt_DB.Text.Trim()))
+                {
+                    throw new Exception("DB Name Is Null Or Empty !");
+                }
+
+                if (string.IsNullOrEmpty(txt_username.Text.Trim()))
+                {
+                    throw new Exception("User Name Is Null Or Empty !");
+                }
+
+                if (string.IsNullOrEmpty(txt_password.Text.Trim()))
+                {
+                    throw new Exception("Password Is Null Or Empty !");
+                }
+
+                GetConnection(txt_serverurl.Text, txt_DB.Text, txt_username.Text, txt_password.Text);
+                GetInnovator();
+
+                Item login_result = mc_conn.Login();
+                if (login_result.isError()) throw new Exception("Login failed, please check connection infomation.");
+
+                SettingConnectionButton(true);
+            }
+            catch (Exception ex)
+            {
+                ShowError("Connection Aras Error:" + ex.Message);
+            }
+        }
+
+        private void btn_disconnection_Click(object sender, EventArgs e)
+        {
+            mc_conn = null;
+            mc_innovator = null;
+            SettingConnectionButton(false);
+            SettingCheckItemButton(false);
+        }
+
+        //------------------------------------------------------------------------------------------------------
+
+        private void btn_Start_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (mc_innovator == null)
+                {
+                    throw new Exception("Please Connection To Aras First.");
+                }
+
+                if (string.IsNullOrEmpty(txt_SearchAML.Text.Trim()))
+                {
+                    throw new Exception("Please Enter Search AML .");
+                }
+
+
+
+                string[] l_getDataRow = txt_DataList.Text.Trim().Split(new char[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+                int l_columnIdx = 0;
+                for (int rowIDX = 0; rowIDX < l_getDataRow.Length; rowIDX++)
+                {
+                    #region process parameter
+                    string[] l_getDataColumn = l_getDataRow[rowIDX].Split(new char[] { '\t' });
+                    if (l_getDataColumn != null && l_getDataColumn.Length != 0)
+                    {
+                        l_columnIdx = l_getDataColumn.Length;
+                    }
+
+                    string l_AML = "";
+                    string l_tempAML = txt_SearchAML.Text.Trim();
+                    for (int columnIDX = 0; columnIDX < l_columnIdx; columnIDX++)
+                    {
+                        l_tempAML = l_tempAML.Replace("$" + (columnIDX + 1), l_getDataColumn[columnIDX]);
+                    }
+
+                    l_AML = "<AML>" + l_tempAML + "</AML>";
+
+                    #endregion
+
+                    #region get item and check
+                    Item l_GetItem = mc_innovator.applyAML(l_AML);
+
+                    if (l_GetItem.isError())
+                    {
+                        throw new Exception("AML Run Have Error[" + l_GetItem.getErrorCode() + "]" + l_GetItem.getErrorDetail());
+                    }
+
+                    if (l_GetItem.getItemCount() == 0)
+                    {
+                        throw new Exception("AML Return Item Zero Error[" + l_GetItem.getErrorCode() + "]" + l_GetItem.getErrorDetail());
+                    }
+
+                    #endregion
+
+                    //SettingProcess(l_getDataRow.Length - 1, rowIDX);
+
+                    //clear node
+                    tre_Item.Nodes.Clear();
+
+                    TreeNode l_rootNode = new TreeNode("Garment Style", 0, 0);
+
+                    #region process garment style
+                    for (int garmentIDX = 0; garmentIDX < l_GetItem.getItemCount(); garmentIDX++)
+                    {
+                        Item l_garmentStyle_Item = l_GetItem.getItemByIndex(garmentIDX);
+
+                        TreeNode l_garmentStyle_Item_Node = new TreeNode(l_garmentStyle_Item.getProperty("item_number", "-"), 0, 0);
+                        l_garmentStyle_Item_Node.Tag = l_garmentStyle_Item.getProperty("item_number", "-");
+
+                        #region process Garment Style Option
+
+                        Item l_GarmentStyleContainsOption_Item = l_GetItem.getRelationships("Garment Style Contains Option");
+                        for (int garOptIDX = 0; garOptIDX < l_GarmentStyleContainsOption_Item.getItemCount(); garOptIDX++)
+                        {
+                            Item l_GarmentStyleOption_Item = l_GarmentStyleContainsOption_Item.getItemByIndex(garOptIDX).getRelatedItem();
+
+                            TreeNode l_GarmentStyleOption_Item_Node = new TreeNode(l_GarmentStyleOption_Item.getProperty("option_no", "option_no") + "<-->" + l_GarmentStyleOption_Item.getProperty("cn_body_pattern", "cn_body_pattern") + "<-->" + l_GarmentStyleOption_Item.getProperty("cn_plu", "cn_plu"), 0, 0);
+
+                            #region process Garment BOM
+
+                            Item l_GarmentStyleOptionBOM = l_GarmentStyleOption_Item.getRelationships("Garment Style Option BOM");
+                            for (int garOptBomIDX = 0; garOptBomIDX < l_GarmentStyleOptionBOM.getItemCount(); garOptBomIDX++)
+                            {
+                                Item l_GarmentBOM_Item = l_GarmentStyleOptionBOM.getItemByIndex(garOptIDX).getRelatedItem();
+
+                                TreeNode l_GarmentBOM_Item_Node = new TreeNode(l_GarmentBOM_Item.getProperty("cn_bom_type", "cn_bom_type"), 0, 0);
+
+                                #region process Garment BOM Part
+
+                                Item l_GarmentBOMPart_Item = l_GarmentBOM_Item.getRelationships("Garment BOM Part");
+                                for (int partIDX = 0; partIDX < l_GarmentBOMPart_Item.getItemCount(); partIDX++)
+                                {
+                                    Item l_part_Item = l_GarmentBOMPart_Item.getItemByIndex(garOptIDX).getRelatedItem();
+
+                                    TreeNode l_part_Node = new TreeNode(l_part_Item.getProperty("item_number", "item_number"), 0, 0);
+
+
+                                    l_GarmentBOM_Item_Node.Nodes.Add(l_part_Node);
+                                }
+
+                                #endregion
+
+                                l_GarmentStyleOption_Item_Node.Nodes.Add(l_GarmentBOM_Item_Node);
+                            }
+
+                            #endregion
+
+                            l_garmentStyle_Item_Node.Nodes.Add(l_GarmentStyleOption_Item_Node);
+                        }
+
+                        #endregion
+
+
+                        l_rootNode.Nodes.Add(l_garmentStyle_Item_Node);
+                    }
+                    #endregion
+
+                    tre_Item.Nodes.Add(l_rootNode);
+                }
+            }
+            catch (Exception ex)
+            {
+                ShowError("check data error:" + ex.Message);
+            }
+        }
+
+        private void btn_StartReplace_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                ShowError("replace data error:" + ex.Message);
+            }
+        }
+    }
+}
